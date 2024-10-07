fa50d4c7db0bc92a2a1a3b79a3fcb47736f7cd85
Aseem Mehrotra <67800004+aseemm84@users.noreply.github.com>
2024-10-07 15:50:37 +0530

    Update backend.py

--- backend.py
+++ backend.py
@@ -11,7 +11,7 @@ def get_llm(temperature):
     Returns an instance of the ChatGroq LLM with the specified temperature.
     """
     return ChatGroq(
-        model="llava-v1.5-7b-4096-preview",
+        model="llama3-70b-8192",
         groq_api_key=groq,
         temperature=temperature
         # other params...
